66d5c30aaf1c16aeed1a8c0318bf12eea3db7d93
Stepan Parunashvili <stepan.p@gmail.com>
2013-12-07 16:29:47 +0800

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Atlas::Application.config.secret_key_base = 'c189ffd2071759213bd54dca7b481a1d73ef01ffd848a8b13b33871949c686fab05b2adeafaa328906331191d61ab843ed82ff4087b15e1e045bd567b194528a'
