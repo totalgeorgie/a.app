33f15af8c6e060c4743e9319e72cdbe82a1dc757
Stepan Parunashvili <stepan.p@gmail.com>
2013-12-09 12:08:59 +0800

    init users

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Atlas::Application.routes.draw do
+  root  "users#new"
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
