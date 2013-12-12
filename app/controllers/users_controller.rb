b727f384838ce816fc6f07ed8698b884f0fbbca8
Stepan Parunashvili <stepan.p@gmail.com>
2013-12-12 15:39:51 +0800

    added ability for user to edit their information. enforced authentication, so only the correct signed in user can see protected pages. also added friendly forwarding

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,8 @@
 class UsersController < ApplicationController
+
+  before_action :signed_in_user, only: [:edit, :update, :show] # makes sure that the user is signed in to access these pages
+  before_action :correct_user, only: [:edit, :update, :show] # makes sure that the user can only edit and see their own info
+
   def new
   	@user = User.new
   end
@@ -18,10 +22,39 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit 
+  end
+
+  def update
+    if @user.update_attributes(user_params)
+      flash[:success] = "Profile Updated"
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+
   private
 
-    def user_params
+    def user_params # this is used in order to restrict what params a person can pass to the controller. i.e otherwise people might pass a parameter for admin=1
       params.require(:user).permit(:name, :email, :password,
                                    :password_confirmation)
+    end
+
+    def signed_in_user
+      unless signed_in?
+        store_location
+        redirect_to signin_url, notice: "Please sign in."
+      end
+    end
+
+    def correct_user 
+      @user = User.find(params[:id])
+      redirect_to(root_url) unless current_user?(@user)
+    end
+
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_url) unless current_user?(@user)
     end
 end
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -12,9 +12,14 @@ module SessionsHelper
   def current_user=(user)
     @current_user = user
   end
+
   def current_user
     remember_token = User.encrypt(cookies[:remember_token])
     @current_user ||= User.find_by(remember_token: remember_token)
+  end
+
+  def current_user?(user)
+    user == current_user
   end
 
   def sign_out
@@ -22,4 +27,14 @@ module SessionsHelper
     cookies.delete(:remember_token)
   end
   
+  def redirect_back_or(default) # this creates friendly forwarding for the app
+    redirect_to(session[:return_to] || default)
+    session.delete(:return_to)
+  end
+
+  def store_location
+    session[:return_to] = request.url if request.get?
+  end
+
+
 end
