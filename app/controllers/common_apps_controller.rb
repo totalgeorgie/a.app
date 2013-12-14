2034cc7c24b59cb3ef7cac0bd18cbbcf8e67a505
Stepan Parunashvili <stepan.p@gmail.com>
2013-12-14 17:35:28 +0800

    moved signed_in_user to sessions helper

--- app/controllers/common_apps_controller.rb
+++ app/controllers/common_apps_controller.rb
@@ -1,2 +1,21 @@
 class CommonAppsController < ApplicationController
+  before_action :signed_in_user
+
+  def create
+    @micropost = current_user.microposts.build(micropost_params)
+    if @micropost.save
+      flash[:success] = "Micropost created!"
+      redirect_to root_url
+    else
+      render 'static_pages/home'
+    end
+  end
+
+
+	def new
+	end
+
+	def create
+	end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Atlas::Application.routes.draw do
   resources :users
   resources :sessions, only: [:new, :create, :destroy]
+  resources :common_apps
   
    root  "users#new"
 
