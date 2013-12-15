512fa88589366341fb47b56a7c1fe711c80e019b
Stepan Parunashvili <stepan.p@gmail.com>
2013-12-15 18:41:34 +0800

    common_app converted to has_one association

--- app/controllers/common_apps_controller.rb
+++ app/controllers/common_apps_controller.rb
@@ -2,15 +2,15 @@ class CommonAppsController < ApplicationController
   before_action :signed_in_user
 
   def new
-    if current_user.common_app.any?
+    if current_user.common_app.present?
       redirect_to current_user
     else
-  		@common_app = current_user.common_app.new 
+      @common_app = current_user.build_common_app
     end
   end
 
   def create 
-  	@common_app = current_user.common_app.build(common_app_params)
+  	@common_app = current_user.build_common_app(common_app_params)
   	if @common_app.save
   		flash[:success] = "Common App Created!"
   		redirect_to root_url
@@ -20,17 +20,15 @@ class CommonAppsController < ApplicationController
   end
 
   def show
-    @common_apps = current_user.common_app
+    @common_app = current_user.common_app
   end
-
-  #for show, edit, we need to make sure that they can only see 1 common_app 
 
   private
 
     def common_app_params
       params.require(:common_app).permit(:current_city,:grad_year,:read_type,
       									  :listen_speak,:time_in_china,
-      									  :cover_letter,:resume)####fill in the correct ones here
+      									  :cover_letter,:resume) ####fill in the correct ones here
     end
 
 end
