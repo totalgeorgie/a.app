1ed84c720e5e81033931bc285a6d8712935da88f
Stepan Parunashvili <stepan.p@gmail.com>
2013-12-17 19:09:19 +0800

    video resource completed

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -24,6 +24,21 @@ class VideosController < ApplicationController
     redirect_to new_video_path, notice: "Looks like you haven't made your video yet! Fill it in below." unless @video.present?
   end   
 
+
+  def update
+    @video = current_user.video
+    if current_user.video.update_attributes(video_params)
+      flash[:success] = "Video App Updated!"
+      redirect_to video_url
+    else
+      render :edit
+    end
+  end
+
+  def edit
+    @video = current_user.video
+  end
+
   private
   
     def video_params
