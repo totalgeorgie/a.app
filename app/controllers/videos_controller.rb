37a04f625d46cb95959a4dddfcd93da0ba326588
Stepan Parunashvili <stepan.p@gmail.com>
2014-01-08 18:01:10 +0100

    updated videos, need to make 100% sure update and create are working

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -2,11 +2,10 @@ class VideosController < ApplicationController
   before_action :signed_in_user
   before_action :correct_user
 
-  # NOTE TEST EVERY SINGLE ACTION HERE! JUST MADE AN UPDATE THAT SHOULD FIX ANY ADMIN ISSUES
-  
+
   def new
     if @user.video
-      redirect_to @user
+      redirect_to edit_user_video_path(@user, @user.video), :notice => "You already created your video. Submit this form to replace your video"
     else
       @video = @user.build_video
     end
@@ -25,7 +24,7 @@ class VideosController < ApplicationController
 
   def show
     @video = @user.video
-    redirect_to new_video_path, notice: "Looks like you haven't made your video yet! Fill it in below." unless @video.present?
+    redirect_to new_user_video_path(@user), notice: "Looks like you haven't made your video yet! Fill it in below." unless @video.present?
   end   
 
 
@@ -35,7 +34,7 @@ class VideosController < ApplicationController
 
     if @video.update_attributes(video_params)
         flash[:success] = "Video Updated!"
-        redirect_to root_url
+        redirect_to @user
     else
         render :new
     end
