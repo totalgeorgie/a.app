2f3f35a5e80b0b416829e58364605f970edbb9fc
Stepan Parunashvili <stepan.p@gmail.com>
2014-01-14 15:50:19 -0500

    application edit and show views

--- app/controllers/applications_controller.rb
+++ app/controllers/applications_controller.rb
@@ -4,14 +4,6 @@ class ApplicationsController < ApplicationController
   before_action :signed_in_user
   before_action :correct_applicant, only: [:show, :edit, :update]  
 
-  # Test the correct applicant
-  def new 
-   job = params[:job_id]
-   @application = Application.build(job)
-   redirect_to jobs_path, :notice => "You've already applied to this job! Check out some more" if has_job(current_user,@job)
-  end
-  # New Action might just be obsolete! 
-
   def create
     @application = Application.new(application_params)
     if @application.save
