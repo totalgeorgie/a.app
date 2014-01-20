f90fd7fbf1b66ef08339997fffc2f2e0ce9b0117
Stepan Parunashvili <stepan.p@gmail.com>
2014-01-20 00:26:59 -0500

    added rudementary heat pciker on user admin view

---
+++ app/assets/javascripts/common_apps.js
@@ -0,0 +1,59 @@
+jQuery(function($) {
+	$.fn.editable.defaults.mode = 'inline';
+    $('.editable').editable();
+
+	$('.editCoverLetter').click(function(e){    
+		   e.preventDefault();
+	       e.stopPropagation();
+	       $('.coverLetter').editable('toggle');
+	});
+
+	$('.editCurrentCity').click(function(e){    
+		   e.preventDefault();
+	       e.stopPropagation();
+	       $('.currentCity').editable('toggle');
+	});
+
+	$('.editGradYear').click(function(e){    
+		   e.preventDefault();
+	       e.stopPropagation();
+	       $('.gradYear').editable('toggle');
+	});
+
+	$('.editTimeInChina').click(function(e){    
+		   e.preventDefault();
+	       e.stopPropagation();
+	       $('.timeInChina').editable('toggle');
+	});
+
+	$('.editReadType').click(function(e){    
+		   e.preventDefault();
+	       e.stopPropagation();
+	       $('.readType').editable('toggle');
+	});
+
+	$('.editListenSpeak').click(function(e){    
+		   e.preventDefault();
+	       e.stopPropagation();
+	       $('.listenSpeak').editable('toggle');
+	});
+
+	$('.editCity').click(function(e){    
+		   e.preventDefault();
+	       e.stopPropagation();
+	       $('.city').editable('toggle');
+	});
+
+	$('.editRole').click(function(e){    
+		   e.preventDefault();
+	       e.stopPropagation();
+	       $('.role').editable('toggle');
+	});
+
+	$('.editIndustry').click(function(e){    
+		   e.preventDefault();
+	       e.stopPropagation();
+	       $('.industry').editable('toggle');
+	});
+
+});
--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,5 +1,6 @@
 class Admin::UsersController < ApplicationController
   #rails g controller Admin::Users index 
+  respond_to :html, :json
   before_action :signed_in_user
   before_action :admin_user
   
@@ -15,8 +16,24 @@ class Admin::UsersController < ApplicationController
     redirect_to users_url 
   end
 
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+       render json: {success: true}
+    else
+       render json: {errors: @user.errors}, status: 400
+    end
+  end
+
+
   def show
     @user = User.find(params[:id])
   end
+
+private
+    def user_params # this is used in order to restrict what params a person can pass to the controller. i.e otherwise people might pass a parameter for admin=1
+      params.require(:user).permit(:name, :email, :password,
+                                   :password_confirmation, :heat_id)
+    end  
 
 end
