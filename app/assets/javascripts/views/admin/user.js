ae4eb1875cecc4c591e51b8e9b0738d10e3646c4
Stepan Parunashvili <stepan.p@gmail.com>
2014-04-13 01:57:30 -0700

    making video show up

--- app/assets/javascripts/views/admin/user.js
+++ app/assets/javascripts/views/admin/user.js
@@ -1,6 +1,10 @@
 AtlasUser.Views.UserShow = Backbone.View.extend({
   template: JST['admin/users/show'],
   
+  initialize: function() {
+    window.setTimeout(CameraTag.setup, 0)
+  },
+
   render: function() {
     var content = this.template({ user: AtlasUser.user })
     this.$el.html(content);
