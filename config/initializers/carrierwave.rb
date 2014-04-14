42b1a8675177a99b0e0541f28cace47e5dce165c
Stepan Parunashvili <stepan.p@gmail.com>
2014-04-14 12:40:56 -0700

    trying endpoint fix

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,9 +1,10 @@
 CarrierWave.configure do |config|
   config.fog_credentials = {
-    :provider              => 'AWS',
-    :aws_access_key_id     => ENV['S3_KEY'],
-    :aws_secret_access_key => ENV['S3_SECRET'],
-    :region                => ENV['S3_REGION']
+    provider:               'AWS',
+    aws_access_key_id:      ENV['S3_KEY'],
+    aws_secret_access_key:  ENV['S3_SECRET'],
+    region:                 ENV['S3_REGION'],
+    endpoint:               ENV['S3_ENDPOINT']
   }
  
   if Rails.env.test? || Rails.env.cucumber?
@@ -16,5 +17,4 @@ CarrierWave.configure do |config|
  
   config.cache_dir = "#{Rails.root}/tmp/uploads"
   config.fog_directory  = ENV['S3_BUCKET_NAME']
-  config.fog_host = ENV['S3_HOST']
 end
