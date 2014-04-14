551ec21d3c088d011888c0e60bc928bec026d1cf
Stepan Parunashvili <stepan.p@gmail.com>
2014-04-14 09:44:56 -0700

    set up S3 for file uploads

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -15,7 +15,5 @@ CarrierWave.configure do |config|
   end
  
   config.cache_dir = "#{Rails.root}/tmp/uploads"
- 
   config.fog_directory    = ENV['S3_BUCKET_NAME']
-  config.s3_access_policy = :public_read
-  config.fog_host         = "#{ENV['S3_ASSET_URL']}/#{ENV['S3_BUCKET_NAME']}"
+end
--- db/migrate/20140414163219_remove_linked_link_from_columns.rb
+++ db/migrate/20140414163219_remove_linked_link_from_columns.rb
@@ -1,4 +1,5 @@
 class RemoveLinkedLinkFromColumns < ActiveRecord::Migration
   def change
+  	remove_column :common_apps, :linked_link
   end
 end
