af2f6e2f25ad6b0d00e60ebb1464f1d0b9fd979f
Stepan Parunashvili <stepan.p@gmail.com>
2014-04-16 00:03:02 -0700

    getting to carrierwave direct

--- app/uploaders/resume_uploader.rb
+++ app/uploaders/resume_uploader.rb
@@ -1,10 +1,6 @@
 # encoding: utf-8
 class ResumeUploader < CarrierWave::Uploader::Base
-  storage :fog
-  
-  def store_dir
-    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
-  end
+  include CarrierWaveDirect::Uploader
   
   def extension_white_list
      %w(doc docx pdf txt jpg png)
