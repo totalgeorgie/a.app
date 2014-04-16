f0f2775a82336c26c3d8be27982e5c23bdede7cf
Stepan Parunashvili <stepan.p@gmail.com>
2014-04-15 23:04:53 -0700

    updating production options

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -40,7 +40,7 @@ Atlas::Application.configure do
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for nginx
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
-  config.force_ssl = true
+  config.force_ssl = false
 
   # Set to :debug to see everything in the log.
   config.log_level = :info
@@ -66,7 +66,7 @@ Atlas::Application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
 
-  config.action_mailer.default_url_options = { :host => "atlas-china.com" }
+  config.action_mailer.default_url_options = { :host => "jobs.atlas-china.com" }
 
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
