81d27d2748c0737a8dc70cac8bdc2647a999dd0e
Stepan Parunashvili <stepan.p@gmail.com>
2014-04-17 13:16:47 -0700

    remove carrierwave_direct

--- app/uploaders/resume_uploader.rb
+++ app/uploaders/resume_uploader.rb
@@ -1,6 +1,10 @@
 # encoding: utf-8
 class ResumeUploader < CarrierWave::Uploader::Base
-  include CarrierWaveDirect::Uploader
+  storage :fog
+   
+  def store_dir
+    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+  end
   
   def extension_white_list
      %w(doc docx pdf txt jpg png)
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140413122246) do
+ActiveRecord::Schema.define(version: 20140414163219) do
 
   create_table "answers", force: true do |t|
     t.integer  "application_id"
