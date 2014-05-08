8eab5f6877098e28f17bc5290616a08715622f9c
Stepan Parunashvili <stepan.p@gmail.com>
2014-05-07 18:45:35 -0700

    tracking video completion

--- app/assets/javascripts/views/video.js
+++ app/assets/javascripts/views/video.js
@@ -15,11 +15,18 @@ Atlas.Views.VideoView = Backbone.AppView.extend({
     Atlas.user.video().set('video_uuid', vid.uuid).save({}, {
       success: function() {
         that.refresh();
+        that.trackVideo();
         Atlas.user.commonApp().fetch();
       }
     });
   },
   
+  trackVideo: function() {
+    _cio.track('video', {
+      completed: true
+    });
+  },
+
   refresh: function() {
     this.render();
     CameraTag.setup();
