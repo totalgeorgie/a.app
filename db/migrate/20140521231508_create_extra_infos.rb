3518579dc4ca3ade4d094aea6156e62fbc6a1582
Stepan Parunashvili <stepan.p@gmail.com>
2014-05-23 13:48:13 -0700

    setting up extra info table

--- db/migrate/20140521231508_create_extra_infos.rb
+++ db/migrate/20140521231508_create_extra_infos.rb
@@ -3,15 +3,16 @@ class CreateExtraInfos < ActiveRecord::Migration
     create_table :extra_infos do |t|
       t.string :linkedin_url
       t.string :source_place
-      t.string :intro
-      t.string :experience_1
-      t.string :experience_2
-      t.string :education_1
-      t.string :education_2
-      t.string :skills
-      t.string :other
-
+      t.string :objective, limit: 800
+      t.string :experience_1, limit: 1000
+      t.string :experience_2, limit: 1000
+      t.string :education_1, limit: 1000
+      t.string :education_2, limit: 1000
+      t.string :other, limit: 500
+      t.integer :user_id, null: :false
       t.timestamps
     end
+
+    add_index :extra_infos, :user_id 
   end
 end
