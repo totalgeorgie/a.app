92efa2790b690317fcc1e29602ad9ee4d0ee55b2
Stepan Parunashvili <stepan.p@gmail.com>
2014-05-23 13:54:43 -0700

    dropped heats

--- app/models/question.rb
+++ app/models/question.rb
@@ -8,17 +8,6 @@
 #  updated_at :datetime
 #  content    :text(500)
 #
-
-# == Schema Information
-#
-# Table name: questions
-#
-#  id         :integer          not null, primary key
-#  job_id     :integer
-#  created_at :datetime
-#  updated_at :datetime
-#  content    :text(500)
-#
 class Question < ActiveRecord::Base
   belongs_to :job
   has_one :answer
