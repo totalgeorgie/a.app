de7a2e9f017bca28eef0c2b0af7b5e85d49c8e44
Stepan Parunashvili <stepan.p@gmail.com>
2014-05-27 22:13:53 -0700

    saving works on profile end

--- app/assets/javascripts/views/career.js
+++ app/assets/javascripts/views/career.js
@@ -1,7 +1,6 @@
 Atlas.Views.CareerView = Backbone.AppView.extend({
   template: JST["career/show"],
   firstFields: JST["career/first-fields"],
-  secondFields: JST["career/second-fields"],
   
   initialize: function() {
     this.user = Atlas.user
@@ -31,16 +30,14 @@ Atlas.Views.CareerView = Backbone.AppView.extend({
     var content = this.template(),
         firstFields = this.firstFields({
           user: Atlas.user
-        }),
-        secondFields = this.secondFields({
-          user: Atlas.user
         });
 
     this.$el.html(content);
-    this.$el.find('.first-fields').append(firstFields)
-    this.$el.find('.second-fields').append(secondFields)
+    this.$el.find('.first-fields').append(firstFields);
+
     window.setTimeout(this._addSelects.bind(this), 0);
     window.setTimeout(this._addSlider.bind(this), 0);
+    
     return this;
   }
 
