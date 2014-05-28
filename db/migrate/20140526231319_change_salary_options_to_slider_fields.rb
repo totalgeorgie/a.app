6f75825c22ea886e14aeb38a235a830f5181db0f
Stepan Parunashvili <stepan.p@gmail.com>
2014-05-28 02:21:23 -0700

    fixing type problem in migration

--- db/migrate/20140526231319_change_salary_options_to_slider_fields.rb
+++ db/migrate/20140526231319_change_salary_options_to_slider_fields.rb
@@ -1,7 +1,7 @@
 class ChangeSalaryOptionsToSliderFields < ActiveRecord::Migration
   def change
-  	rename_column :common_apps, :ideal_salary, :salary_pref
-  	change_column :common_apps, :salary_pref, :integer
+  	remove_column :common_apps, :ideal_salary
+  	create_column :common_apps, :salary_pref, :integer
     add_column :common_apps, :culture_pref, :integer
   end
 end
