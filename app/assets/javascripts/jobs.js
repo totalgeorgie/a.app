aea7343e96ef0b8070762ccec3069e5f62ae744a
Stepan Parunashvili <stepan.p@gmail.com>
2014-05-27 17:42:14 -0700

    writing code for sliders on career

--- app/assets/javascripts/jobs.js
+++ app/assets/javascripts/jobs.js
@@ -1,14 +1,14 @@
 jQuery(function($){
-	$('.application_form').on('submit', function(event){
-		var $form = $(this);
-		var num_questions = parseInt($form.find('#questions_count').val());
-		
-		for (var i = 0; i < num_questions; i++) {
-			var video_uuid = $form.find('#' + i + "_video_uuid").val();
-			if (!!video_uuid) {
-				var $answer_uuid = $($form.find('#application_answers_attributes_' + i + '_video_uuid'));
-				$answer_uuid.val(video_uuid);
-			}
-		};
-	});
+  $('.application_form').on('submit', function(event){
+    var $form = $(this);
+    var num_questions = parseInt($form.find('#questions_count').val());
+    
+    for (var i = 0; i < num_questions; i++) {
+      var video_uuid = $form.find('#' + i + "_video_uuid").val();
+      if (!!video_uuid) {
+        var $answer_uuid = $($form.find('#application_answers_attributes_' + i + '_video_uuid'));
+        $answer_uuid.val(video_uuid);
+      }
+    };
+  });
 });
