eec9e2c924ea60d9aa4b9a1c698b079fa49e1866
Stepan Parunashvili <stepan.p@gmail.com>
2014-06-01 19:48:30 -0700

    set up role type

--- app/models/role_type.rb
+++ app/models/role_type.rb
@@ -1,2 +1,10 @@
 class RoleType < ActiveRecord::Base
+  has_many :common_app_role_type_relations, dependent: :destroy 
+  has_many :common_apps, through: :common_app_role_type_relations
+  has_many :job_role_type_relations, dependent: :destroy 
+  has_many :jobs, through: :job_role_type_relations
+
+  def self.preload
+    RoleType.select(:id, :name).all.map{|ind| { id: ind.id.to_s, name: ind.name } }
+  end
 end
