a37544d0d6a90ce27ee79802f9404dbc1fdf7cf7
Stepan Parunashvili <stepan.p@gmail.com>
2014-06-19 01:59:43 -0400

    remove x-editable stuff

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,17 +3,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   include SessionsHelper
 
-  def xeditable? object = nil
-    xeditable = params[:denied] ? false : true
-    can?(:edit, object) and xeditable ? true : false
-  end
-
-  helper_method :xeditable?
-
-  def can? edit, model
-    true
-  end
-
   private
   def expire_hsts
     response.headers['Strict-Transport-Security'] = 'max-age=0'
